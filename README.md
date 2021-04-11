a0f9f3c46cdc28234857e55a1ffff6cf4883eb7c
mdecourse <mdecourse@gmail.com>
2021-04-11 17:19:05 +0800

    add 6 axis robotic arm related papers

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# cmstemplate
+#  Project 4082
+
+Repository: https://github.com/mdecourse/pj4082
+
+Github Pages: https://mde.tw/pj4082
+
+Heroku: https://pj4082.herokuapp.com
--- init.py
+++ init.py
@@ -10,7 +10,7 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "CMSiMDE"
+    site_title = "Project 4082"
     # for IPv4
     ip = "127.0.0.1"
     # for IPv6
